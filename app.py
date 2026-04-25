651980e78a077e7106b216335af424d964200feb
William <wavrusawi@gmail.com>
2026-04-25 06:18:33 -0500

    Added SQLAlchemy support

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import render_template, Flask
+from flask_sqlalchemy import SQLAlchemy # type: ignore
 
-app = Flask(__name__)
+app = Flask(__name__) # 1. Create the app first!
+
+# 2. Then set the config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://app:TNLkZk5a@localhost/eeducate'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Highly recommended to save memory
+
+# 3. Finally, initialize the database with the app
+db = SQLAlchemy(app)
 
 @app.route('/')
 def roleSelect():
     return render_template('index.html')
+
 @app.route('/educator')
 def educatorLogin():
     return render_template('educatorLogin.html')
