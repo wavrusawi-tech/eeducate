f90209a4d23731e10d88e5b194aebba3103fcc81
William <wavrusawi@gmail.com>
2026-04-26 13:03:10 -0500

    Added Admin Class

--- login/models.py
+++ login/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class EEducateAdmin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(maxLength=20, unique=True)
+    authorized_apps = models.JSONField(default=list)
