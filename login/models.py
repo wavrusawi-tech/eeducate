6953d7bd8a954b29e6f978c810a8bbfaa0b3e505
William <wavrusawi@gmail.com>
2026-04-26 13:08:25 -0500

    Bug fix

--- login/models.py
+++ login/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class EEducateAdmin(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(maxLength=20, unique=True)
+    username = models.CharField(max_length=20, unique=True)
     authorized_apps = models.JSONField(default=list)
     def __str__(self):
         return self.user.username
