3980a7743227471219b517bf92e58191d6ded151
William <wavrusawi@gmail.com>
2026-04-26 13:07:43 -0500

    Added method

--- login/models.py
+++ login/models.py
@@ -6,3 +6,6 @@ class EEducateAdmin(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(maxLength=20, unique=True)
     authorized_apps = models.JSONField(default=list)
+    def __str__(self):
+        return self.user.username
+    
