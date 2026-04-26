6a769fa5a9d46083eb6d66ea80d87e2bca1f9b55
William <wavrusawi@gmail.com>
2026-04-26 14:54:27 -0500

    Added a welcome page

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,9 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
+app_name="login"
+
 urlpatterns = [
     path('roles/', views.role_select, name='roleView'),
+    path('', views.welcome, name='welcomeView')
 ]
--- login/views.py
+++ login/views.py
@@ -3,3 +3,5 @@ from django.contrib.auth.decorators import login_required
 
 def role_select(request):
     return render('request', 'login/role.html')
+def welcome(request):
+    return render('request', 'login/index.html')
